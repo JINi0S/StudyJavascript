e41a2314138953c4c32fb254c943f2009cb3d5c2
LeeJinHee <wlsgml8847@kakao.com>
2025-03-07 22:05:48 +0900

    [Ref] 주석 제거

--- Assignment/day4-quiz/script.js
+++ Assignment/day4-quiz/script.js
@@ -56,11 +56,9 @@ function displayQuiz() {
 
 function playGame(button, selectedAnswer) {
     if (quizzes[playIdx].answer === selectedAnswer) {
-        // gameBackground.style.backgroundColor = "#00ff00"; // 정답 → 초록색
         bg.style.backgroundColor = "#00ff00";
         button.classList.add('correct');
     } else {
-        // gameBackground.style.backgroundColor = "#ff0000"; // 오답 → 빨간색
         bg.style.backgroundColor = "#ff0000";
         button.classList.add('wrong');
     }
@@ -91,79 +89,3 @@ function restartGame() {
 }
 
 displayQuiz();
-
-
-
-/*
-let playIdx = 0;
-
-const quizTitle = document.getElementById('quiz-title');
-quizTitle.textContent = "ㄹ ㄴ"
-
-const nextBtn = document.getElementById('next');
-const retryBtn = document.getElementById('restart');
-
-function endGame() {   
-    // 게임 결과 텍스트 업데이트
-    const gameContainer = document.getElementById('game-container');
-    gameContainer.style.color = "#505050"; // 회색 변경
-    retryBtn.style.display = 'block';  // 재도전 버튼 활성
-}
-
-// 마지막 퀴즈면 restart, 아니면 next
-// 정답 맞추면 초록색, 틀리면 빨간색
-
-function playGame(selectedAnswer) {
-    nextBtn.style.display = 'block';  // 넥스트 버튼 보이기
-
-    const gameContent = document.getElementById('game-content');
-    if (quizzes[playIdx].answer == selectedAnswer) {
-        // 정답 초록색 처리
-        gameContent.style.backgroundColor = "#00ff00";
-    } else {
-       // 오답 빨간색 처리
-       gameContent.style.backgroundColor = "#ff0000";
-
-    }    
-}
-
-function onNext() {
-    playIdx += 1;
-    // 회색색상으로 변경
-    // 게임 보여주기??
-    if (playIdx === quizzes.length) {
-        endGame()
-    }
-}
-
-function restartGame() {
-    // 남은 횟수 초기화
-    playIdx = 0;
-    
-    // document.getElementById('remainCount').innerText = `${remainCount}`;
-
-    nextBtn.style.display = 'nonw'; 
-
-    // 게임 종료 뷰 초기화
-    retryBtn.style.display = 'none';  // 게임 종료 비활성
-}
-
-// 퀴즈 렌더링
-function displayMenu(quizzes) {
-    const menuContainer = document.getElementById('quiz-title');
-    
-    // 메뉴 항목들을 HTML로 변환
-    const menuHTML = quizzes.map(item => `
-            <div class="quiz-info">
-                <h3>${item.title}</h3>
-                <div class="game-button" id="filterButtons">
-                    <button onclick="playGame('가위')">가위</button>
-                    <button onclick="playGame('바위')">바위</button>
-                </div>
-            </div>
-    `).join('');
-
-    menuContainer.innerHTML = menuHTML;
-}
-displayMenu(quizzes)
-*/
