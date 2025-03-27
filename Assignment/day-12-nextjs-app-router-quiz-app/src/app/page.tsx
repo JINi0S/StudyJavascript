34ee84d5e911c1ee0585702323cd25d3f1bd4b1b
LeeJinHee <wlsgml8847@kakao.com>
2025-03-27 17:09:06 +0900

    [Add] 12일차 - 퀴즈 앱 (nextjs app router)

---
+++ Assignment/day-12-nextjs-app-router-quiz-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-7xl text-center text-blue-600 pb-5">
+        CS QUIZ
+      </h1>
+    </div>
+  );
+}
